6e39f4d77cf3de44a95c35997fe056864b13fa86
Gustavo <guh-51@hotmail.com>
2024-04-09 09:11:27 -0300

    feat: Component Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Post } from "./Post";
+import { Post } from "./components/Post";
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
 import styles from "./App.module.css";
 import "./Global.css";
-import { Sidebar } from "./components/Sidebar";
+
 function App() {
   return (
     <div>
