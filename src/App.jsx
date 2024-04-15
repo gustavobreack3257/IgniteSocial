08e6d9e546b825ab88b0c971bc90e537759bc112
Gustavo <guh-51@hotmail.com>
2024-04-15 14:45:48 -0300

    feat: iteração com jsx - Propriedades do Post

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import { Sidebar } from "./components/Sidebar";
 import styles from "./App.module.css";
 import "./Global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/gustavobreack3257.png',
+      name: 'Gustavo costa souza',
+      role: 'Analista desenvolvedor',
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galera 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no curso da Rocketseat la naa trilha ignite. O nome do projeto é rocketSocial 🚀'},
+      {type: 'Link', content: 'gustavoboy.dev/rocketseatsocial'},
+    ],
+    publishedAt: new Date('2024-04-15 10:45:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-04-15 10:45:00'),
+  },
+]
+
 function App() {
   return (
     <div>
@@ -13,9 +44,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
           <main>
-            <Post author="Gustavo costa souza" content="blablabla" />
-
-            <Post author="Editar perfil" content="blablabla" />
+            {posts.map(post => {
+              return (
+                <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+                />
+              )
+            })}
           </main>
       </div>
     </div>
