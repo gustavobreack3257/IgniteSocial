4fed21c4ee43b3508edab89cda17850360f127d4
Gustavo <guh-51@hotmail.com>
2024-04-19 10:11:08 -0300

    feat: inserindo key

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,36 +1,44 @@
 /* eslint-disable react/prop-types */
-import styles from './Comment.module.css'
+import styles from "./Comment.module.css";
 
-import {Avatar} from './Avatar'
-import { ThumbsUp, Trash } from '@phosphor-icons/react'
+import { Avatar } from "./Avatar";
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
 
-export function Comment({content}){
-    return(
-        <div className={styles.comment}>
-            <Avatar src='https://github.com/gustavobreack3257.png' hasBorder={false}/>
-            <div className={styles.commentBox}>
-                <div className={styles.commentContent}>
-                    <header>
-                        <div className={styles.authorAndTime}>
-                            <strong>Gustavo costa souza</strong>
-                            <time title='9 de abril ás 14h' dateTime='2024-04-09 14:00:00'>Cerca de 1 hora atrás</time>
-                        </div>
-
-                        <button>
-                            <Trash size={24}/>
-                        </button>
-                    </header>
-
-                    <p>{content}</p>
-                </div>
-
-                <footer>
-                    <button>
-                        <ThumbsUp/>
-                        Aplaudir <span>20</span>
-                    </button>
-                </footer>
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        src="https://github.com/gustavobreack3257.png"
+        hasBorder={false}
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gustavo costa souza</strong>
+              <time title="9 de abril ás 14h" dateTime="2024-04-09 14:00:00">
+                Cerca de 1 hora atrás
+              </time>
             </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
         </div>
-    )
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -38,6 +38,10 @@ export function Post({ author, publishedAt, content }) {
   function handleNewCommentChange(event) {
     setNewCommentText(event.target.value);
   }
+
+  function deleteComment(comment) {
+    console.log(`Deletar comentário ${comment}`);
+  }
   return (
     <article className={styles.post}>
       <header>
@@ -58,10 +62,10 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === "link") {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="#">{line.content}</a>
               </p>
             );
@@ -86,7 +90,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment content={comment}/>;
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          );
         })}
       </div>
     </article>
